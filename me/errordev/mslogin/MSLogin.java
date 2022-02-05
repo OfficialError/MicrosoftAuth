4182ffb88b7d060c38a8fb98a3b0b1f7aaa3dfab
OfficialError <45919406+OfficialError@users.noreply.github.com>
2022-02-05 18:02:21 +0100

    Added .gitignore and updated the MSLogin class
    Added .gitignore to ignore files
    Added a stop method to the MSLogin.java

--- me/errordev/mslogin/MSLogin.java
+++ me/errordev/mslogin/MSLogin.java
@@ -10,46 +10,57 @@ public enum MSLogin {
     INSTANCE;
     private HttpServer server;
     private MSLoginHandler handler;
+    private Thread serverThread;
 
     public void setHandler(MSLoginHandler handler) {
         this.handler = handler;
     }
 
     public void start() {
-        try {
-            server = HttpServer.create(new InetSocketAddress(5500), 5500);
-            server.start();
-            server.createContext("/auth", (exchange) -> {
-                final Headers headers = exchange.getRequestHeaders();
-                final String uuid, username, accessToken;
-                if (handler == null) {
-                    String response = "Authentication handler is null";
-                    exchange.sendResponseHeaders(403, response.getBytes().length);
-                    OutputStream os = exchange.getResponseBody();
-                    os.write(response.getBytes());
-                    os.close();
-                    return;
-                }
-                if ((uuid = headers.getFirst("uuid")) != null &&
-                        (username = headers.getFirst("username")) != null &&
-                        (accessToken = headers.getFirst("accessToken")) != null) {
-                    String response = "Successfully authenticated";
-                    exchange.sendResponseHeaders(200, response.getBytes().length);
-                    OutputStream os = exchange.getResponseBody();
-                    os.write(response.getBytes());
-                    os.close();
-                    handler.onLoginSuccess(uuid, username, accessToken);
-                } else {
-                    String response = "Unsuccessfully authenticated";
-                    exchange.sendResponseHeaders(405, response.getBytes().length);
-                    OutputStream os = exchange.getResponseBody();
-                    os.write(response.getBytes());
-                    os.close();
-                }
-            });
-        } catch (Exception e) {
-            throw new RuntimeException("Failed to start server", e);
-        }
+        serverThread = new Thread(() -> {
+            try {
+                server = HttpServer.create(new InetSocketAddress(5500), 5500);
+                server.start();
+                server.createContext("/auth", (exchange) -> {
+                    final Headers headers = exchange.getRequestHeaders();
+                    final String uuid, username, accessToken;
+                    if (handler == null) {
+                        String response = "Authentication handler is null";
+                        exchange.sendResponseHeaders(403, response.getBytes().length);
+                        OutputStream os = exchange.getResponseBody();
+                        os.write(response.getBytes());
+                        os.close();
+                        return;
+                    }
+                    if ((uuid = headers.getFirst("uuid")) != null &&
+                            (username = headers.getFirst("username")) != null &&
+                            (accessToken = headers.getFirst("accessToken")) != null) {
+                        String response = "Successfully authenticated";
+                        exchange.sendResponseHeaders(200, response.getBytes().length);
+                        OutputStream os = exchange.getResponseBody();
+                        os.write(response.getBytes());
+                        os.close();
+                        handler.onLoginSuccess(uuid, username, accessToken);
+                    } else {
+                        String response = "Unsuccessfully authenticated";
+                        exchange.sendResponseHeaders(405, response.getBytes().length);
+                        OutputStream os = exchange.getResponseBody();
+                        os.write(response.getBytes());
+                        os.close();
+                    }
+                });
+            } catch (Exception e) {
+                throw new RuntimeException("Failed to start server", e);
+            }
+        });
+        server.start();
+    }
+
+    public void stop() {
+        serverThread.interrupt();
+        server.stop(0);
+        serverThread = null;
+        server = null;
     }
 
 }
